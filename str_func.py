30e81d6930afda6fd28bb92772afd069cc6dac74
Rasuljka <ras199167@gmail.com>
2023-11-20 21:25:44 +0300

    add str_capitalize

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_upper():
     user_input = input("Введите слово, я преобразую все буквы к заглавным")
     return user_input.upper()
+
+
+def str_capitalize():
+    """Преобразует первые буквы к заглавным"""
+    user_input = input("Введите слово, я преобразую первые буквы к заглавным")
+    return user_input.capitalize()
